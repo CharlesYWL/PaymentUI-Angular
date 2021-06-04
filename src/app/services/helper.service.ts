0463305193b953a9dbbfb4ae98123872017f78dd
CharlesYWL <xlyin@ucdavis.edu>
2021-06-04 14:56:36 -0700

    optimize cart.component logic

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -9,4 +9,9 @@ export class HelperService {
     let i = parseInt(cents);
     return '$' + i / 100 + '.' + String(i % 100).padEnd(2, '0');
   }
+  objectKeysLen(obj: any) {
+    if (typeof obj === 'object' && obj !== null) {
+      return Object.keys(obj).length;
+    } else return 0;
+  }
 }
